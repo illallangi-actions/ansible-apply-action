eaeabf53945f29d4ce7ffe5283938c43d7bd7550
Andrew Cole <andrew.cole@illallangi.com>
2020-02-19 00:05:11 +0000

    Added ansible-galaxy and ansible-playbook to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+source=${SOURCE:-source}/
+
+echo cd ${SOURCE}
+cd ${SOURCE}
+
+echo ansible-galaxy install --role-file ./requirements.yaml
+ansible-galaxy install --role-file ./requirements.yaml
+
+echo ansible-playbook ./main.yaml
+ansible-playbook ./main.yaml
