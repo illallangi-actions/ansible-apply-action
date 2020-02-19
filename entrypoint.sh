2d2d580a07f584c827dc0512dfc0b620ec1b5b0c
Andrew Cole <andrew.cole@illallangi.com>
2020-02-19 01:01:22 +0000

    Added private key handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ -z "${SSH_PRIVATE_KEY}" ]]; then
+  echo mkdir -p "${HOME}/.ssh"
+  mkdir -p "${HOME}/.ssh"
+  echo echo "${SSH_PRIVATE_KEY}" \> "${HOME}/.ssh/id_rsa"
+  echo "${SSH_PRIVATE_KEY}" > "${HOME}/.ssh/id_rsa"
+fi
+
 echo cd "${GITHUB_WORKSPACE}"
 cd "${GITHUB_WORKSPACE}"
 
